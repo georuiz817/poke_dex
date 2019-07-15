d2d6d748ebfb20163fbf1aa42d57ced17c35ab5e
George Ruiz <georuiz817@gmail.com>
2019-07-15 05:11:00 +0000

    additional

--- lib/poke_dex/dex.rb
+++ lib/poke_dex/dex.rb
@@ -9,6 +9,7 @@ end
 
 def self.scrape_bulbapedia 
 doc = Nokogiri::HTML(open("https://pokemon.fandom.com/wiki/Bulbasaur"))
+
 dex_1 = self.new 
 dex_1.name = doc.search("h1.page-header__title").text
 dex_1.gen = doc.search("#mw-content-text > aside > section:nth-child(3) > section:nth-child(1) > section.pi-smart-group-body > div > a").text
@@ -16,10 +17,12 @@ dex_1.type =doc.search("span.t-type.type-grass").first.text
 dex_1.height= doc.search("#mw-content-text > aside > section:nth-child(7) > section:nth-child(2) > section.pi-smart-group-body > div:nth-child(1) > span").text
 dex_1.weight = doc.search("#mw-content-text > aside > section:nth-child(7) > section:nth-child(1) > section.pi-smart-group-body > div:nth-child(2) > span").text
 
+
 #divider
 #######
 
 doc_2= Nokogiri::HTML(open("https://pokemon.fandom.com/wiki/Pikachu"))
+
 dex_2=self.new 
 dex_2.name = doc_2.search("h1.page-header__title").first.text
 dex_2.gen = doc_2.search("#mw-content-text > aside > section:nth-child(3) > section:nth-child(1) > section.pi-smart-group-body > div > a").text
@@ -31,15 +34,13 @@ dex_2.weight = doc_2.search("#mw-content-text > aside > section:nth-child(7) > s
 #####
 
 doc_3 = Nokogiri::HTML(open("https://pokemon.fandom.com/wiki/Shuckle"))
+
 dex_3 = self.new 
 dex_3.name = doc_3.search("h1.page-header__title").first.text
 dex_3.gen = doc_3.search("#mw-content-text > p:nth-child(2) > a:nth-child(8)").text
 dex_3.type = doc_3.search("span.t-type2").first.text
-dex_3.height =doc_3.search("")
-
-
-
-
+dex_3.height = doc_3.search("#mw-content-text > aside > section:nth-child(7) > section:nth-child(2) > section.pi-smart-group-body > div:nth-child(1) > span").text 
+dex_3.weight = doc_3.search("#mw-content-text > aside > section:nth-child(7) > section:nth-child(1) > section.pi-smart-group-body > div:nth-child(2) > span").text
 
 [dex_1,dex_2,dex_3]
 end
