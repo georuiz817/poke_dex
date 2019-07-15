c630f0cb7d170e1e405dcca4b74d12ac9c1d20a6
George Ruiz <georuiz817@gmail.com>
2019-07-15 05:50:04 +0000

    COMPLETE 2nd COMMIT

--- lib/poke_dex/cli.rb
+++ lib/poke_dex/cli.rb
@@ -11,10 +11,12 @@ include PokeDex::Extra
  def list_pokemon
    @pokemons = PokeDex::Dex.scrape_pokemons 
      @pokemons.each do |dex_1,dex_2,dex_3,dex_4| 
-     puts "1.#{dex_1.name}"
-     puts "2.#{dex_2.name}"
-     puts "3.#{dex_3.name}"
-     puts "4.#{dex_4.name}"
+     puts <<~DOC
+     1.#{dex_1.name}
+     2.#{dex_2.name}
+     3.#{dex_3.name}
+     4.#{dex_4.name}
+     DOC
   end
  end
  
